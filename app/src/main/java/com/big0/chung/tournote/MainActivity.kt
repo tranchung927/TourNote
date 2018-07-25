b39628563475fb610d3a2c077607b939f8ef2594
ChungTV <tranchung927@gmail.com>
2018-07-25 09:06:08 +0700

    add actionbar

--- app/src/main/java/com/big0/chung/tournote/MainActivity.kt
+++ app/src/main/java/com/big0/chung/tournote/MainActivity.kt
@@ -2,6 +2,9 @@ package com.big0.chung.tournote
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +13,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_actions, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search -> {
+                Toast.makeText(this, "Search button selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.help -> {
+                Toast.makeText(this, "Help button selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.about -> {
+                Toast.makeText(this, "About button selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
